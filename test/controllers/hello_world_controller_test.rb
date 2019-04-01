24e62daace967658410c63f430ed32d096fd8e5b
Zhenya Telyukov <telyukov@gmail.com>
2019-04-01 15:56:42 +0300

    initial

---
+++ test/controllers/hello_world_controller_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+#class HelloWorldControllerTest < ActionDispatch::IntegrationTest
+#  test "should get index" do
+#    get hello_world_index_url
+#    assert_response :success
+#  end
+#
+#end
+
+# See:  https://www.rubydoc.info/gems/react-rails/2.5.0
+
+class HelloWorldControllerTest < ActionDispatch::IntegrationTest
+  test 'assert_react_component' do
+    get "/hello_world/index.html"
+    assert_equal 200, response.status
+
+    # assert rendered react component and check the props
+    assert_react_component "HelloWorld" do |props|
+      assert_equal "Hello from react-rails.", props[:greeting]
+      assert_equal "react-rails", props[:info][:name]
+      assert_select "[class=?]", "hello-world"
+    end
+
+    # or just assert component rendered
+    assert_react_component "HelloWorld"
+  end
+end
